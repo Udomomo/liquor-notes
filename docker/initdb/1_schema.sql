08e5b27cf8bf8b4548c25c7ad67ae0c36be13b9a
Udomomo <udomomo@gmail.com>
2023-06-11 12:11:29 +0900

    fix schema

--- docker/initdb/1_schema.sql
+++ docker/initdb/1_schema.sql
@@ -1,5 +1,5 @@
 create table if not exists users (
-    id bigint auto_increment primary key,
+    id bigint primary key,
     username varchar(50) unique not null,
     password varchar(50) not null,
     email varchar(100) unique not null,
@@ -8,7 +8,7 @@ create table if not exists users (
 );
 
 create table if not exists reviews (
-    id bigint auto_increment primary key,
+    id bigint primary key,
     user_id bigint not null,
     title varchar(100) not null,
     content varchar(1000),
@@ -18,14 +18,14 @@ create table if not exists reviews (
 );
 
 create table if not exists tags (
-    id bigint auto_increment primary key,
+    id bigint primary key,
     name varchar(50) not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
 );
 
 create table if not exists review_tags (
-    id bigint auto_increment primary key,
+    id bigint primary key,
     review_id bigint not null,
     tag_id bigint not null,
     created_at timestamp default current_timestamp,
