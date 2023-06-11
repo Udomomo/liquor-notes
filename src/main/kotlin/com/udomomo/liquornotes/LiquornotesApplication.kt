29f836f6bd647e870e0f8d34ef0f560c9d7c9cc5
Udomomo <udomomo@gmail.com>
2023-06-11 11:01:51 +0900

    initial commit

---
+++ src/main/kotlin/com/udomomo/liquornotes/LiquornotesApplication.kt
@@ -0,0 +1,11 @@
+package com.udomomo.liquornotes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiquornotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<LiquornotesApplication>(*args)
+}
---
+++ src/test/kotlin/com/udomomo/liquornotes/LiquornotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.udomomo.liquornotes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiquornotesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
