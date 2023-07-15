d742c1747d0e2b387fad3c15fec52e7106115add
Udomomo <udomomo@gmail.com>
2023-07-15 11:51:32 +0900

    fix domain

--- src/main/kotlin/com/udomomo/liquornotes/domains/Review.kt
+++ src/main/kotlin/com/udomomo/liquornotes/domains/Review.kt
@@ -1,22 +1,27 @@
 package com.udomomo.liquornotes.domains
 
+import com.udomomo.liquornotes.ids.Id
+
 class Review private constructor(
+    val userId: Id,
     val title: String,
     val contents: String,
     val star: Star,
-    val tagIds: List<Long>,
+    val tags: List<Tag>,
 ) {
     companion object {
         fun of(
+            userId: Id,
             title: String,
             contents: String,
             star: Star,
-            tagIds: List<Long>,
+            tags: List<Tag>,
         ): Review {
             if (title.length > 100) throw IllegalArgumentException("title is too long")
             if (contents.length > 1000) throw IllegalArgumentException("contents is too long")
+            if (tags.size > 10) throw IllegalArgumentException("tag is too many")
 
-            return Review(title, contents, star, tagIds)
+            return Review(userId, title, contents, star, tags)
         }
     }
 }
--- src/test/kotlin/com/udomomo/liquornotes/domains/ReviewTest.kt
+++ src/test/kotlin/com/udomomo/liquornotes/domains/ReviewTest.kt
@@ -1,5 +1,6 @@
 package com.udomomo.liquornotes.domains
 
+import com.udomomo.liquornotes.ids.Id
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -8,12 +9,13 @@ class ReviewTest {
     @Test
     fun `should create review successfully`() {
         // Arrange
+        val userId = Id("testId") // TODO: use mock
         val title = "title".repeat(20)
         val contents = "contents".repeat(125)
         val star = Star.of(5.0)
 
         // Do
-        val review = Review.of(title, contents, star, listOf(1L, 2L))
+        val review = Review.of(userId, title, contents, star, listOf(Tag.of("tag01"), Tag.of("tag02")))
 
         // Verify
         assertEquals(title, review.title)
@@ -22,12 +24,22 @@ class ReviewTest {
     @Test
     fun `should fail creating review with too long title`() {
         // Arrange
+        val userId = Id("testId") // TODO: use mock
         val title = "title".repeat(20) + "a"
         val contents = "contents".repeat(125)
         val star = Star.of(5.0)
 
         // Do
-        val error = assertThrows<IllegalArgumentException> { Review.of(title, contents, star, listOf(1L, 2L)) }
+        val error =
+            assertThrows<IllegalArgumentException> {
+                Review.of(
+                    userId,
+                    title,
+                    contents,
+                    star,
+                    listOf(Tag.of("tag01"), Tag.of("tag02")),
+                )
+            }
 
         // verify
         assertEquals("title is too long", error.message)
@@ -36,14 +48,40 @@ class ReviewTest {
     @Test
     fun `should fail creating review with too long contents`() {
         // Arrange
+        val userId = Id("testId") // TODO: use mock
         val title = "title".repeat(20)
         val contents = "contents".repeat(125) + "a"
         val star = Star.of(5.0)
 
         // Do
-        val error = assertThrows<IllegalArgumentException> { Review.of(title, contents, star, listOf(1L, 2L)) }
+        val error =
+            assertThrows<IllegalArgumentException> {
+                Review.of(
+                    userId,
+                    title,
+                    contents,
+                    star,
+                    listOf(Tag.of("tag01"), Tag.of("tag02")),
+                )
+            }
 
         // verify
         assertEquals("contents is too long", error.message)
+    }
+
+    @Test
+    fun `should fail creating review with too many tags`() {
+        // Arrange
+        val userId = Id("testId") // TODO: use mock
+        val title = "title".repeat(20)
+        val contents = "contents".repeat(125)
+        val star = Star.of(5.0)
+        val tags = (1..11).map { Tag.of("tag$it") }
+
+        // Do
+        val error = assertThrows<IllegalArgumentException> { Review.of(userId, title, contents, star, tags) }
+
+        // verify
+        assertEquals("tag is too many", error.message)
     }
 }
