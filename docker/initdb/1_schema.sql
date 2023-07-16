8791f071306cf3c64d9c90c153c9e61658f17776
Udomomo <udomomo@gmail.com>
2023-07-16 11:26:27 +0900

    update schema

--- docker/initdb/1_schema.sql
+++ docker/initdb/1_schema.sql
@@ -1,5 +1,5 @@
 create table if not exists users (
-    id varchar(24) primary key,
+    id varchar(26) primary key,
     username varchar(50) unique not null,
     password varchar(50) not null,
     email varchar(100) unique not null,
@@ -8,8 +8,8 @@ create table if not exists users (
 );
 
 create table if not exists reviews (
-    id varchar(24) primary key,
-    user_id bigint not null,
+    id varchar(26) primary key,
+    user_id varchar(26) not null,
     title varchar(100) not null,
     content varchar(1000),
     stars decimal not null,
@@ -18,16 +18,16 @@ create table if not exists reviews (
 );
 
 create table if not exists tags (
-    id varchar(24) primary key,
+    id varchar(26) primary key,
     name varchar(50) not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
 create table if not exists review_tag_mappings (
-    id varchar(24) primary key,
-    review_id bigint not null,
-    tag_id bigint not null,
+    id varchar(26) primary key,
+    review_id varchar(26) not null,
+    tag_id varchar(26) not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
     index idx_review_tags_review_id (review_id),
