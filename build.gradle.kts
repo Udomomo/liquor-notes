3c308f7a46c46aeb2b798e7f05063e7a0655fbb0
Udomomo <udomomo@gmail.com>
2023-08-15 00:24:02 +0900

    Add exposed spring-transaction

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
         implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
         implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
         implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+        implementation("org.jetbrains.exposed:spring-transaction:$exposedVersion")
         implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
     }
 
