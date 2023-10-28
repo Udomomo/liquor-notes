97e19433484d79dacbffe0e92c4c4bbdda6a5cf5
Udomomo <udomomo@gmail.com>
2023-10-28 11:57:48 +0900

    create header

--- frontend/src/app/components/global-navigation/global-navigation.component.ts
+++ frontend/src/app/components/global-navigation/global-navigation.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-global-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './global-navigation.component.html',
   styleUrls: ['./global-navigation.component.scss']
 })
