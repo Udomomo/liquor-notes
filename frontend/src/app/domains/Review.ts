59b0e13dc2da3d2e3fcdce272ab923fee311c082
Udomomo <udomomo@gmail.com>
2023-10-30 17:30:12 +0900

    pass temporarl data to review

---
+++ frontend/src/app/domains/Review.ts
@@ -0,0 +1,25 @@
+export interface Review {
+  id: string,
+  user: User,
+  title: string,
+  star: number,
+  contents: string,
+  createdAt: string,
+  location: Location,
+  tags: Tag[]
+}
+
+export interface User {
+  id: string,
+  name: string
+}
+
+export interface Location {
+  id: string,
+  name: string
+}
+
+export interface Tag {
+  id: string,
+  name: string
+}
