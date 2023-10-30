1d323139a47052f1e6223e7035c5c659abd5c603
Udomomo <udomomo@gmail.com>
2023-10-30 19:33:08 +0900

    align contents to center

--- frontend/src/app/components/review/review.component.ts
+++ frontend/src/app/components/review/review.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Review } from 'src/app/domains/Review';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.scss']
 })
